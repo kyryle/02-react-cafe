57ce250f1a5945e88b2752bacae05df1322b2070
user <kyryle0110@gmail.com>
2026-08-03 18:13:44 +0300

    check

---
+++ src/components/App/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+import css from "./App.module.css";
+import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import type { Votes } from "../../types/Votes";
+
+const votes: Votes = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+console.log(votes);
+
+export default function App() {
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      <VoteOptions onVote='cat' onReset='' canReset={true}/>
+      
+      </div>
+  );
+}
+// function handleVote(type) {
+//     const [clicks, setClicks] = useState(0)
+// }
+
+// function resetVotes() {
+
+// }
---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,11 @@
+import css from "./VoteOptions.module.css";
+
+export default function VoteOptions({onVote, onReset, canReset}) {
+  return (
+  <div className={css.container}>
+    <button className={css.button}>Good</button>
+    <button className={css.button}>Neutral</button>
+    <button className={css.button}>Bad</button>
+    <button className={`${css.button} ${css.reset}`}>Reset</button>
+  </div>);
+}
