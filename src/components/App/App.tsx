7e8d97b4aaefa7b7bfc90a3778f3c2a8976f65ad
user <kyryle0110@gmail.com>
2026-08-03 18:35:34 +0300

    text

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
+import type { Votes } from "../../types/Votes";
 
 const votes: Votes = {
   good: 0,
