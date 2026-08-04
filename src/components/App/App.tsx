7e1e36b6e7bc0ed1416e1dd0ff32808a8ed1f9cf
user <kyryle0110@gmail.com>
2026-08-04 21:14:39 +0300

    check

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
-// import { useState } from "react";
 
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo";
 import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 import type { Votes } from "../../types/votes";
+
 
 const votes: Votes = {
   good: 0,
@@ -12,19 +14,14 @@ const votes: Votes = {
 };
 console.log(votes);
 
+
 export default function App() {
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions onVote='cat' onReset='' canReset={true}/>
-      
+      <VoteOptions onVote="good" onReset='' canReset={true}/>
+      <VoteStats votes = {votes} totalVotes = {0} positiveRate = {0}/>
+      <Notification/>
       </div>
   );
 }
-// function handleVote(type) {
-//     const [clicks, setClicks] = useState(0)
-// }
-
-// function resetVotes() {
-
-// }
--- src/components/VoteOptions/VoteOptions.tsx
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -1,20 +1,41 @@
 import css from "./VoteOptions.module.css";
+import type { VoteType } from "../../types/votes";
+import { useState } from "react";
+// import VoteStats from "../VoteStats/VoteStats";
 
-interface options {
-  onVote: string,
-  onReset: string,
-  canReset: boolean
+export interface VoteOptionsProps {
+  onVote: VoteType;
+  onReset: string;
+  canReset: boolean;
 }
 
+export default function VoteOptions({
+  onVote,
+  onReset,
+  canReset,
+}: VoteOptionsProps) {
+  const [clicks, setClicks] = useState(0);
 
-export default function VoteOptions({onVote, onReset, canReset}: options) {
-  return (
-  <div className={css.container}>
-    <button className={css.button} >Good</button>
-    <button className={css.button}>Neutral</button>
-    <button className={css.button}>Bad</button>
-    <button className={`${css.button} ${css.reset}`}>Reset</button>
-    </div>);
-  console.log(onVote, onReset, canReset);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setClicks(clicks + 1);
+    console.log(event);
+  };
+
+  // const resetVotes = () => { };
   
+  return (
+    <div className={css.container}>
+      <button className={css.button} onClick={handleClick}>
+        Good
+      </button>
+      <button className={css.button} onClick={handleClick}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={handleClick}>
+        Bad
+      </button>
+      <button className={`${css.button} ${css.reset}`}>Reset</button>
+    </div>
+  );
+  console.log(onVote, onReset, canReset);
 }
