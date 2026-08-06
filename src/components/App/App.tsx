9d8cbd7670cb0ed61279627917f1d7897fd35193
user <kyryle0110@gmail.com>
2026-08-06 15:53:27 +0300

    done

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,13 +8,6 @@ import type { Votes, VoteType } from "../../types/votes";
 import { useState } from "react";
 
 
-// const clicks: Votes = {
-//   good: 0,
-//   neutral: 0,
-//   bad: 0,
-// };
-
-
 export default function App() {
   const [clicks, setClicks] = useState<Votes>({good: 0, neutral: 0, bad: 0});
 
